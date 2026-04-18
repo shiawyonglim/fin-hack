589692a6097c45a74b81b2c5253dee82bdf38193
Ember Tio <embertio@gmail.com>
2026-04-19 05:13:35 +0800

    filter for high-risk transaction types

---
+++ clean_paysim.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df = pd.read_csv('paysim_database.csv')
+
+df.columns = [col.lower() for col in df.columns]
+df = df.rename(columns={
+    'oldbalanceorg': 'old_balance_orig',
+    'newbalanceorig': 'new_balance_orig',
+    'namedest': 'name_dest',
+    'oldbalancedest': 'old_balance_dest',
+    'newbalancedest': 'new_balance_dest'
+})
+
+df = df.drop(columns=['nameorig', 'namedest'])
+
+df_cleaned = df[df['type'].isin(['TRANSFER', 'CASH_OUT'])].copy()
+
+df_cleaned.to_csv('ai_engine/data/CLEAN_paysim.csv', index=False)
+
+print("Success! PaySim data is now cleaned and saved in ai_engine/data.")
