27b42e499f8ffeafd827132cac0a6c93afb0cb59
shiawyonglim <shiawyonglim@gmail.com>
2026-04-20 09:01:12 +0800

    deleted some whisper residules

--- public/ai_engine/models/whisper-tiny-onnx/clean_subreddit.py
+++
@@ -1,3 +0,0 @@
-input_file = 'datasets/malaysia_subreddit_comments.csv'
-
-output_file = 'datasets/CLEAN_malaysia_slang_v1.csv'
--- public/ai_engine/scripts/generate_audio.ps1
+++
@@ -1,5 +0,0 @@
-Add-Type -AssemblyName System.Speech
-$synth = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$synth.SetOutputToWaveFile("c:\finhack\fin-hack\ai_engine\scripts\sample_voice.wav")
-$synth.Speak("Transfer two hundred and fifty ringgit to Mr. Loy.")
-$synth.Dispose()
